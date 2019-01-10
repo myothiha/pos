f6ba0f170623867975a38122436bef7c54bab08f
myothiha <myothiha576@gmail.com>
2019-01-10 14:04:25 +0630

    Sale Report Stage 1

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Sale;
 use App\StockIn;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -11,15 +12,34 @@ class ReportController extends Controller
     public function stockInReport()
     {
         $stockIns = StockIn::all();
-        
+
         return view('admin.report.stockInReport', [
             'stockIns' => $stockIns,
         ]);
     }
 
-   	public function saleReport()
+    public function saleReport(Request $request)
     {
         $sales = Sale::all();
+
+        return view('admin.report.saleReport', [
+            'sales' => $sales,
+        ]);
+    }
+
+    public function saleReportFilter(Request $request)
+    {
+        $sales = Sale::query()
+            ->when($request->saleType, function ($q) use ($request) {
+                return $q->where('saleType', '=', $request->saleType);
+            })
+            ->when($request->daterange, function ($q) use ($request) {
+                $dateRange = explode(' - ', $request->daterange);
+                $from = Carbon::parse($dateRange[0]);
+                $to = Carbon::parse($dateRange[1]);
+                return $q->whereDate('created_at','>=', $from)
+                    ->whereDate('created_at','<=', $to);
+            })->get();
 
         return view('admin.report.saleReport', [
             'sales' => $sales,
